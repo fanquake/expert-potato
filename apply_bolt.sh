6b2cd373feff5407fd792057d114d047a2daa51f
fanquake <fanquake@gmail.com>
2026-07-10 15:33:01 +0100

    Add apply_bolt

---
+++ apply_bolt.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BITCOIND="build/bin/bitcoind"
+BOLT="$(pwd)/llvm_toolchain/bin/llvm-bolt"
+DATA="${1:-$(pwd)/bolt_data/prof.fdata}"
+
+BOLT_ARGS=(
+  -dyno-stats
+  -reorder-functions=cdsort
+  -reorder-blocks=ext-tsp
+  -split-functions
+  -split-all-cold
+  -split-eh
+  --align-blocks
+  --peepholes=all
+  --icf=safe
+  --inline-all
+  --inline-memcpy
+  --plt=hot
+  --tail-duplication=aggressive
+)
+
+$BOLT $BITCOIND -o bitcoind.bolt -data=$DATA ${BOLT_ARGS[*]}
